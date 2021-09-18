cde7d9e4e64167cf5358931344444738954f2001
amit1011 <amitmathur1011@gmail.com>
2021-09-18 19:22:22 +0530

    This is Wrapper Class Example.

---
+++ Wrapper/force-app/main/default/aura/Wrapper/WrapperController.js
@@ -0,0 +1,20 @@
+({
+    initData : function(component, event, helper) {
+        var a= component.get("v.recordId");
+        console.log('***In helper'+a);
+        //call apex class method
+        var action = component.get('c.getAccountWithContacts');
+          action.setParams({
+              accountId : component.get("v.recordId")
+          });
+        action.setCallback(this, function(response) {
+          //store state of response
+          var state = response.getState();
+          if (state === "SUCCESS") {
+            //set response value in wrapperList attribute on component.
+            component.set('v.accountContactWrapper', response.getReturnValue());
+          }
+        });
+        $A.enqueueAction(action);
+    }
+})
